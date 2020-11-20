d238a590fb1093263ecb3d674f0cd6a88b8ad21e
Vadim Novash <novash@tut.by>
2020-11-20 16:14:00 +0300

    add common utils functions
    - getRandomInteger
    - getRandomBoolean
    - shuffle array

---
+++ src/utils/common.js
@@ -0,0 +1,22 @@
+const getRandomInteger = (a = 0, b = 1) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+
+const getRandomBoolean = () => Math.random() <= 0.5;
+
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+export {
+  getRandomInteger,
+  getRandomBoolean,
+  shuffleArray,
+};
