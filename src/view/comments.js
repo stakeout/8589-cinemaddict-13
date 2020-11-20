9dd09f3dc2849758ca0f23682c21bb8b13ebae28
Vadim Novash <novash@tut.by>
2020-11-20 14:08:11 +0300

    add commens and comment item view template

---
+++ src/view/comments.js
@@ -0,0 +1,8 @@
+export const createCommentsTemplate = () => (
+  `<section class="film-details__comments-wrap">
+    <h3 class="film-details__comments-title">Comments
+      <span class="film-details__comments-count">4</span>
+    </h3>
+    <ul class="film-details__comments-list"></ul>
+  </section>`
+);
